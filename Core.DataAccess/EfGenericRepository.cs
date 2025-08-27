9e21a85d5961c09448b5aadc3ed2cb99860f1624
Meryem Aydın <aydnn.meryem@gmail.com>
2025-08-27 19:53:05 +0300

    fixed a misunderstanding

--- Core.DataAccess/EfGenericRepository.cs
+++ Core.DataAccess/EfGenericRepository.cs
@@ -50,42 +50,6 @@ namespace Core.DataAccess
             return filter == null ? _dbSet : _dbSet.Where(filter);
             // return filter == null ? _dbSet.AsQueryable() : _dbSet.Where(filter).AsQueryable();
         }
-        public async Task AddAsync(T entity)
-        {
-            _dbSet.AddAsync(entity);
-            await Context.SaveChangesAsync();
-        }
-
-        public async Task UpdateAsync(T entity)
-        {
-            _dbSet.Update(entity);
-            await Context.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(T entity)
-        {
-            _dbSet.Remove(entity);
-            await Context.SaveChangesAsync();
-        }
-
-        public async Task<T> GetAsync(Expression<Func<T, bool>> filter)
-        {
-            return await _dbSet.FirstOrDefaultAsync(filter);
-
-        }
-
-        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null)
-        {
-            return filter == null
-                ? await _dbSet.ToListAsync()
-                : await _dbSet.Where(filter).ToListAsync();
-        }
-
-        public IQueryable<T> GetQueryableAsync(Expression<Func<T, bool>>? filter = null)
-        {
-            return filter == null
-                ? _dbSet
-                : _dbSet.Where(filter);
-        }
+        
     }
 }
